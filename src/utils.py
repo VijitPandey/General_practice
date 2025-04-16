68ea71596374a7ad9b8b4ae77b53eced3210f322
Vijit-Pandey <vijitsp30@gmail.com>
2025-04-16 21:46:25 +0530

    Model Trainer Done

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ from src.exception import CustomException
 from src.logger import logging
 import dill
 
+from sklearn.metrics import r2_score
+
 def save_object(file_path, obj):
     """
     Save the object to a file using pickle.
@@ -18,6 +20,24 @@ def save_object(file_path, obj):
             dill.dump(obj, file)
         
         logging.info(f"Object saved at {file_path}")
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate the models and return the best model based on R2 score.
+    """
+    try:
+        report = {}
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2_score = model.score(X_test, y_test)
+            report[model_name] = r2_score
+        
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
