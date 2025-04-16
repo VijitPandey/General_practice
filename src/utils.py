22cc17ae5865b9f1f381611afb3959a66aac8ef7
Vijit-Pandey <vijitsp30@gmail.com>
2025-04-16 17:11:02 +0530

    data Transformation done

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os 
+import sys
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+from src.logger import logging
+import dill
+
+def save_object(file_path, obj):
+    """
+    Save the object to a file using pickle.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file:
+            dill.dump(obj, file)
+        
+        logging.info(f"Object saved at {file_path}")
+
+    except Exception as e:
+        raise CustomException(e, sys)
